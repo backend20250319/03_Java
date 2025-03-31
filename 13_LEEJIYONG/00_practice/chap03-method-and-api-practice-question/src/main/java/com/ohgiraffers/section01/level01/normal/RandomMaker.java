c49c3370cf0f410d014c61898f9de7fc9d188842
gyongcode <gyongcode@gmail.com>
2025-03-31 09:00:36 +0900

    자바 과제

---
+++ 13_LEEJIYONG/00_practice/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/normal/RandomMaker.java
@@ -0,0 +1,55 @@
+package com.ohgiraffers.section01.level01.normal;
+
+public class RandomMaker {
+
+    /*
+     * static 메소드로 생성
+     * 반환형 : String
+     * 메소드명 : rockPaperScissors
+     *
+     * 가위,바위,보 중에 하나를 출력
+     * (가위,바위,보 는 난수를 이용하여 구현)
+     *
+     * -- 출력 --
+     * 가위
+     * */
+    public static String rockPaperScissors() {
+        int n = (int) (Math.random() * 3);
+        if (n == 0) {
+            System.out.println("가위");
+            return "가위";
+        }
+        if (n == 1) {
+            System.out.println("바위");
+            return "바위";
+        }
+
+        System.out.println("보");
+        return "보";
+    }
+
+    /*
+     * static 메소드로 생성
+     * 반환형 : String
+     * 메소드명 : tossCoin
+     *
+     * 동전 앞/뒤 중에 하나를 출력
+     * (앞면,뒷면은 난수를 이용하여 구현)
+     *
+     * -- 출력 --
+     * 앞면
+     *
+     * */
+    public static String tossCoin() {
+        int n = (int) (Math.random() * 2);
+
+        if (n == 0) {
+            System.out.println("앞면");
+            return "앞면";
+        }
+
+        System.out.println("뒷면");
+        return "뒷면";
+    }
+
+}
